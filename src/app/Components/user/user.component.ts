0068761621001c349e10f9320054e718717cb818
Yanik Kumar <yanikkumar@cdac.in>
2024-08-01 12:29:08 +0530

    Department and city list added

--- src/app/Components/user/user.component.ts
+++ src/app/Components/user/user.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { UserModel } from '../../Model/user';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  user: UserModel = {
+    department: '',
+    name: '',
+    mobile: '',
+    email: '',
+    gender: '',
+    doj: '',
+    city: '',
+    salary: 0,
+    address: '',
+    status: false,
+  };
 
+  cityList: string[] = ['Chandigarh', 'Delhi', 'Mohali', 'Gurgaon'];
+  departmentList: string[] = ['IT', 'HR', 'Accounts', 'Sales', 'Management'];
+
+  onSubmit(form: NgForm): void {}
 }
